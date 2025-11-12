40eba5de15c2663b8085bb277d25a643b1bcde7a
Claude-Mug <claudemug4@gmail.com>
2025-11-12 15:51:58 +0200

    Fix case sensitivity in auth controller import

--- routes/auth/auth.route.js
+++ routes/auth/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../../controllers/auth/auth.Controller');
+const authController = require('../../controllers/auth/auth.controller');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
