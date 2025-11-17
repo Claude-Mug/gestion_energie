c5234976de8c175073161f541c0d5208d99fd4c3
Claude-Mug <claudemug4@gmail.com>
2025-11-17 14:33:48 +0200

    Enhance db.js with SSL options for MySQL
    Added SSL configuration for database connection.

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,8 +9,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA)
+      }
+    },
+    logging: false
   }
 );
 
